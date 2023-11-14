9326cb1b1673af55b8bcd3cf8a44b35563f0e56b
Fallenwood <fallenwood.y@outlook.com>
2023-11-14 19:35:53 +0800

    tmp

--- src/pages/home/file/File.tsx
+++ src/pages/home/file/File.tsx
@@ -6,15 +6,33 @@ import { objStore } from "~/store"
 import { Download } from "../previews/download"
 import { OpenWith } from "./open-with"
 import { getPreviews } from "../previews"
+import axios from "axios"
+
+const fetchVisits = async (filename: string) => {
+  try {
+    const response = await axios.post(
+      `/danmakuhub/visit?filename=${encodeURIComponent(filename)}`,
+    )
+    return response.data.visits
+  } catch {
+    return -1
+  }
+}
 
 const File = () => {
   const previews = createMemo(() => {
     return getPreviews({ ...objStore.obj, provider: objStore.provider })
   })
   const [cur, setCur] = createSignal(previews()[0])
+  const [visits, setVisits] = createSignal(-1)
+  fetchVisits(objStore.obj.name).then((v) => setVisits(v))
+
   return (
     <Show when={previews().length > 1} fallback={<Download openWith />}>
       <VStack w="$full" spacing="$2">
+        <div>
+          <span>Total Visits: {visits()}</span>
+        </div>
         <HStack w="$full" spacing="$2">
           <SelectWrapper
             alwaysShowBorder
